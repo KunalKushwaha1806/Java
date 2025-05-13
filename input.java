cc3878bc19221240333419d6a5d0e71a27e8197c
kunalkushwaha1806 <kunalkushwaha1806@gmail.com>
2025-05-13 22:09:00 +0530

    update

--- input.java
+++ input.java
@@ -5,15 +5,21 @@ public class input {
         System.out.println("Enter the number:");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number 1:");
-        int a = sc.nextInt();
+            System.out.println("Enter the number 1:");
+            int a = sc.nextInt();
 
-        System.out.println("Enter the number 2:");
-        int b = sc.nextInt();
+            System.out.println("Enter the number 2:");
+            int b = sc.nextInt();
 
-        int c = a + b;
-        System.out.println("Sum is: " + c);
+            int c = a + b;
+            System.out.println("Sum is: " + c);
 
+            // Clear the buffer before reading the full sentence
+            sc.nextLine(); // consume leftover newline
+
+            System.out.println("Enter a full sentence:");
+            String wt = sc.nextLine();
+            System.out.println("You entered: " + wt);
         sc.close();
     }
 }
