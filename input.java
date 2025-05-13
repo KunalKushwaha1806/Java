7554e89fb484e4a276097fa1244a6e033e08ec59
kunalkushwaha1806 <kunalkushwaha1806@gmail.com>
2025-05-13 21:55:36 +0530

    input from user

---
+++ input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number 1:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the number 2:");
+        int b = sc.nextInt();
+
+        int c = a + b;
+        System.out.println("Sum is: " + c);
+
+        sc.close();
+    }
+}
